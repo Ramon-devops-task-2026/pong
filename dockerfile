19ccbc8abebe5d72c8ae34be578857158f5912e2
ramglat-cpu <-->
2026-01-24 19:44:09 +0200

    add envirement

--- dockerfile
+++ dockerfile
@@ -1,10 +1,13 @@
 FROM python:3.12-slim
 
+#מיקום  של התיקייה הראשית הראשונית שאתה נכנס לקונטיינר
 WORKDIR /app
 
+#מהתיקייה במחשב לתיקייה בקונטיינר 
 COPY src/requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY src/ ./src/
 
+#פקודה ראשונה שתעבוד שכשמפעילים את הקונטיינר
 CMD ["python", "src/main.py"]
--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 import os
-import time
 from flask import Flask, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
 
-
-
-
-# Environment variable (from Docker / docker-compose)
+# Environment variable
 ENV_NAME = os.getenv("ENV_NAME", "unknown")
+
+print(f"[{datetime.now()}] 🚀 PONG service started | ENV_NAME={ENV_NAME}")
 
 @app.route('/pong', methods=['GET'])
 def pong():
-    print(f"[{datetime.now()}] Pong request received")
+    print(f"[{datetime.now()}] 🔁 PONG({ENV_NAME}) received request")
 
     return jsonify({
         "service": "pong",
@@ -22,12 +20,15 @@ def pong():
         "timestamp": datetime.now().isoformat()
     })
 
+
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify({
-        "status": "healthy",
-        "service": "pong"
+        "service": "pong",
+        "env": ENV_NAME,
+        "status": "healthy"
     })
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
